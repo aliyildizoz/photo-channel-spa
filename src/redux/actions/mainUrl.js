ffa0285bd9ed42e1b34e44f2c50c96ecb8b9764e
Ali Yıldızöz <aliyildizoz909@gmail.com>
2024-03-26 04:59:52 +0300

    changed base url by env

--- src/redux/actions/mainUrl.js
+++ src/redux/actions/mainUrl.js
@@ -1,1 +1,6 @@
-export const  MAIN_URL = "https://localhost:44367";
+var mainUrl="http://localhost:500"
+if (process.env.NODE_ENV==='production') {
+    mainUrl=`http://${process.env.REACT_APP_MAIN_URL}`;
+}
+export const  MAIN_URL = mainUrl;
+console.log(MAIN_URL);
